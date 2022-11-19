c9001e71fa56ba94f9fd3d5d30467c1f26d48c04
Pruthvi Shetty <pruthvishetty5656@gmail.com>
2022-11-19 15:50:03 +0530

    Adds basic setup,lambda function

---
+++ randomMarkdownFunction/index.js
@@ -0,0 +1,38 @@
+var { faker } = require("@faker-js/faker");
+
+function generateMD() {
+  var fileName = faker.lorem.word() + "-" + faker.lorem.word();
+  var fileContents = `---
+  title: "${faker.lorem.words()}"
+  layout: Page
+  ---
+  ${faker.lorem.paragraphs()}
+  `;
+  return {
+    fileName: fileName,
+    body: fileContents,
+  };
+}
+
+function generatePosts(count) {
+  const arr = [];
+  for (var i = 0; i < count; i++) {
+    arr.push(generateMD());
+  }
+  return arr;
+}
+
+exports.handler = async (event) => {
+  console.log(event?.queryStringParameters, "THIS IS EVEnt PARAMS");
+  const markDownCount = event?.queryStringParameters?.count || 1;
+  const markdownPosts = generatePosts(markDownCount);
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
+    },
+    body: JSON.stringify(markdownPosts),
+  };
+  return response;
+};
